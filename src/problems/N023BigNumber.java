a6864f70cdc58e37a4ed121ec27e56f141020edb
GoodDonkey <president20500@gmail.com>
2022-04-16 20:30:52 +0900

    023: BigNumber solution

--- src/problems/N023BigNumber.java
+++ src/problems/N023BigNumber.java
@@ -15,49 +15,34 @@ public class N023BigNumber {
     void test01() {
         String solution = new N023BigNumber().solution("1924", 2);
         assertEquals(solution, "94");
-        
     }
     
     public String solution(String number, int k) {
         String answer = "";
         int nonK = number.length() - k;
-        
-        List<int[]> indexes = getCombs(number.length(), nonK);
-        ArrayList<String> list = new ArrayList<>();
-        
-        for (int[] index : indexes) {
-            StringBuilder sb = new StringBuilder();
-            for (int i : index) {
-                sb.append(number.charAt(i));
-            }
-            list.add(sb.toString());
-        }
-        
         int max = 0;
-        for (String sb : list) {
-            int now = Integer.parseInt(sb);
-            if (now > max) {
-                max = now;
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nonK; i++) { // nonK 자리를 뽑을 것이다.
+            max = 0; // 현 loop 에서 가장 큰 숫자를 뽑기 위한 기준
+            for (int j = index; j <= k + i; j++) {
+                // 지난번 가장 큰 숫자의 다음 인덱스부터 탐색함
+                // 앞으로 찾아야 할 최소 자리의 숫자는 남겨둔 범위까지만 탐색함
+                int target = Character.getNumericValue(number.charAt(j));
+                if (target == 9) {
+                    max = 9;
+                    index = j + 1;
+                    break;
+                }
+                if (max < target) {
+                    max = target;
+                    index = j + 1;
+                }
             }
+            sb.append(max);
         }
-        answer = String.valueOf(max);
+        answer = sb.toString();
         
         return answer;
-    }
-    public List<int[]> getCombs(int n, int r) {
-        List<int[]> combinations = new ArrayList<>();
-        combination(combinations, new int[r], 0, n - 1, 0);
-        return combinations;
-    }
-    
-    private void combination(List<int[]> combinations, int[] data, int start, int end, int index) {
-        if (index == data.length) {
-            int[] combination = data.clone();
-            combinations.add(combination);
-        } else if (start <= end) {
-            data[index] = start;
-            combination(combinations, data, start + 1, end, index + 1);
-            combination(combinations, data, start + 1, end, index);
-        }
     }
 }
