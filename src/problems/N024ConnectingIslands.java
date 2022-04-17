e2ce71466d4c0680d0fc7eae9e4d5deb0cf19122
GoodDonkey <president20500@gmail.com>
2022-04-17 18:50:14 +0900

    024: Connecting Islands with Kruskal

--- src/problems/N024ConnectingIslands.java
+++ src/problems/N024ConnectingIslands.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,44 +22,29 @@ public class N024ConnectingIslands {
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        Stack<Integer> nodes = new Stack<>();
-        nodes.push(0);
-        
-        Stack<Integer> cost = new Stack<>();
-        
-        int index = 0;
-        while (index < n) {
-            int target = nodes.peek() + 1;
-            if (costs[index][1] == target) {
-                if (costs[index][0] < costs[index][1]) {
-                    nodes.push(costs[index][1]);
-                    cost.push(costs[index][2]);
-                }
-                
-            } else if (costs[index][1] == target - 1) {
-                if (costs[index][0] < costs[index][1]) {
-                    if (costs[index][2] < cost.peek()) {
-                        cost.push(costs[index][2]);
-                    }
-                }
-            }
-            index++;
+        // 가중치 기준으로 정렬
+        int[] rank = new int[n];
+        int[] root = new int[n];
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
         }
+        System.out.println(Arrays.toString(rank));
+        System.out.println(Arrays.toString(root));
+        Arrays.sort(costs, Comparator.comparing(a -> a[2]));
+        System.out.println(Arrays.deepToString(costs));
         
-        answer = cost.stream().mapToInt(Integer::intValue).sum();
-        
+        int[] finalCosts = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (root[costs[i][0]] != root[costs[i][1]]) {
+                finalCosts[i] = costs[i][2];
+                root[costs[i][1]] = root[costs[i][0]];
+            }
+        }
+        System.out.println(Arrays.toString(finalCosts));
+        answer = Arrays.stream(finalCosts).sum();
         return answer;
     }
     
     /* 비용의 합을 구하라
-    * 0-1, 1-2, 0-2,
-    * Stack
-    * 각 노드에서 2가지 대안 중 cost 가 더 낮은 것을 계속 선택해 나간다.
-    * 최종적으로 n 개의 노드를 담아야 한다.
-    *
-    * nodes{0, 1, 2, 3}  cost{1, 2, 1}
-    * [0,1,1] [0,2,2] [1,2,5] [1,3,1] [2,3,8]
-    * 0 있으면 담아 1있으면 담아 cost도 담아
-    * 2 있으면 담아, first가 2보다 작아? cost 담아
-    * 3있으면 담아, fist가 3보다 작아? cost가 내 cost보다 작아? */
+    *  */
 }
