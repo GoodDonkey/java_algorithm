a15ace4d1ce764d517a8292c4b5e34b1e4390610
GoodDonkey <president20500@gmail.com>
2022-05-19 15:12:59 +0900

    019: Tiredness solution

--- src/problems/N019Tiredness.java
+++ src/problems/N019Tiredness.java
@@ -1,20 +1,13 @@
 package problems;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class N019Tiredness {
-    
-    private List<List<String>> perms = new ArrayList<>();
-    
     
     @Test
     @DisplayName("test01")
@@ -25,33 +18,25 @@ public class N019Tiredness {
     
     }
     
-    public int solution(int k, int[][] dungeons) {
-        int answer = 0;
-        // 최소 필요 피로도가 큰 순서대로 돌아야 함.
-        // 다음 던전을 돌 수 없는 경우, 최근 던전을 취소하고 (최소는 항상 만족하므로)
-        // 소모 피로도가 가장 작은것 or 최근 던전을 간신히 돌 수 있는 던전을 돌면 된다.
-        // 그런데 던전 개수가 많이 주어지지 않으므로 모두 탐색하는 것도 괜찮다.
-        List<String> arr = new ArrayList<>();
-        for (int i = 0; i < dungeons.length; i++) {
-            arr.add(String.valueOf(i));
-        }
-        recursion(arr, new ArrayList<>(), dungeons.length, dungeons.length);
-        System.out.println(perms);
+    public static boolean visited[];
+    public static int ans = 0;
     
-        return answer;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return 0;
     }
     
-    private void recursion(List<String> arr, List<String> result, int n, int r) {
-        
-        if (r == 0) {
-            perms.add(result);
+    private void dfs(int tired, int[][] dungeons, int count) {
+        for (int i = 0; i < dungeons.length; i++) {
+            // 이 for 문은 너무 많이 돈다. 개선 방안이 없을까??
+            // 던전 3개면 27번임
+            if (!visited[i] && dungeons[i][0] <= tired) {
+                visited[i] = true;
+                dfs(tired - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
         }
-        
-        for (int i = 0; i < n; i++) {
-            
-            result.add(arr.remove(i));
-            recursion(arr, result, n - 1, r - 1);
-            arr.add(i, result.remove(result.size() - 1));
-        }
+        ans = Math.max(ans, count);
     }
 }
